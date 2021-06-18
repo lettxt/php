f211a2716c23282d1477ddffc3478027d45ab043
tom <tom@sapletta.com>
2021-06-18 22:00:46 +0200

    Update github docs

--- .apibuild/server.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-echo I will start PHP serverfor this project ...
-WHERE node
-IF %ERRORLEVEL% NEQ 0 (
-    echo php is not installed on this system!
-) else (
-    ::node server.js
-    forever start server.js
-)
--- .apibuild/stop.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-echo I will stop the php application ...
-::tasklist /v | find "node.exe"
-taskkill /F /IM node.exe
